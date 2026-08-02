0f1f63ebf99d6c08ffe3391382997d36f059f1cf
anton dvinyaninov <antondvinyaninov@icloud.com>
2026-08-02 13:30:15 +0400

    fix: run NFI strategy by default on stable

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,5 +4,5 @@ python /freqtrade/scripts/gpt_advisor.py &
 if [ "$#" -gt 0 ]; then
     exec "$@"
 else
-    exec freqtrade trade --logfile /freqtrade/user_data/logs/freqtrade.log --config /freqtrade/user_data/config.json --strategy SmartFreqaiStrategy --freqaimodel LightGBMRegressor
+    exec freqtrade trade --logfile /freqtrade/user_data/logs/freqtrade.log --config /freqtrade/user_data/config.json --config /freqtrade/user_data/nfi-live.json --strategy NostalgiaForInfinityX7
 fi
