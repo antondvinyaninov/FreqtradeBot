49b4bb853979e8f874210211a9a0bab54c331314
anton dvinyaninov <antondvinyaninov@icloud.com>
2026-08-02 13:44:06 +0400

    fix: force NFI execution without arguments

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 set -eu
 python /freqtrade/scripts/gpt_advisor.py &
-if [ "$#" -gt 0 ]; then
-    exec "$@"
-else
-    exec freqtrade trade --logfile /freqtrade/user_data/logs/freqtrade.log --config /freqtrade/user_data/config.json --config /freqtrade/user_data/nfi-live.json --strategy NostalgiaForInfinityX7
-fi
+exec freqtrade trade --logfile /freqtrade/user_data/logs/freqtrade.log --config /freqtrade/user_data/config.json --config /freqtrade/user_data/nfi-live.json --strategy NostalgiaForInfinityX7
